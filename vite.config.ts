dd0c1ceb86eb293cc0ac011d418819269ba71d92
luisart3 <luisgerardosart@gmail.com>
2026-03-22 07:05:26 -0600

    Fix: corregir ruta de estilos para producción

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import tailwindcss from '@tailwindcss/vite'
+import tailwind from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/bentoui/',
-  plugins: [vue(), tailwindcss()],
+  plugins: [vue(), tailwind()],
 })
