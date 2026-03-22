999faa2339f21064a2b9b960cf8d087b46336462
luisart3 <luisgerardosart@gmail.com>
2026-03-21 19:53:14 -0600

    Chore: configurar base path para deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
-  base: 'https://github.com/luisart3/bentoui',
+  base: '/bentoui/',
 })
